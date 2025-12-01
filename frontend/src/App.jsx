fe41810abfcf4d50233038eab9053e426c4c9114
li <2871253570@qq,com>
2025-12-01 00:44:47 +0000

    init fronend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Welcome</h1>
+        <p>Frontend is ready</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
