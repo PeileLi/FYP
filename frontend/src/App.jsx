44a1c8343c9c0db0b92aa636e9a2d9863d9b5622
Li <2871253570@qq.com>
2025-12-16 18:16:55 +0000

    fix error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './home';
 import './App.css';
-import Home from './Home';
+
 import Login from './login';
 
 function App() {
